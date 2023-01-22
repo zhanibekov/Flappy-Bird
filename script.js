74733e95d046e6166d03707d450225dec4b9364b
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-01-22 17:41:24 +0600

    добавил логику keyup и keydown

--- script.js
+++ script.js
@@ -65,4 +65,24 @@ function play() {
     requestAnimationFrame(move);
 
 
+    let bird_dy = 0;
+
+    function apply_gravity() { // Гравитация//
+        if (game_state != 'Play') return;
+        bird_dy = bird_dy + gravity;
+        document.addEventListener('keydown', (e) => {
+            if (e.key == 'ArrowUp' || e.key == ' ') {
+                img.src = 'images/Bird-2.png';
+                bird_dy = -7.6;
+            }
+        });
+
+        document.addEventListener('keyup', (e) => {
+            if (e.key == 'ArrowUp' || e.key == ' ') {
+
+            }
+        })
+
+    }
+
 }
