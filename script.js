4569f064eb6c36d7f88d1f54acf9693087ecf9d4
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-01-22 19:56:20 +0600

    created variables and other...

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let move_speed = 3,
     gravity = 0.5;
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+
+let bird_props = bird.getBoundingClientRect();
+let background = document.querySelector('.background').getBoundingClientRect();
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'Start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
